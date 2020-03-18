524285cfd425635b7a3f94e18ade1fe328c864d6
Mykola.Somakin <k.semakin@gmail.com>
2020-03-18 17:20:13 +0200

    Updates

---
+++ precache-manifest.4a68015307de0c968f4ccf253b9b068e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bc904f2113ad26b627884b7e10cae6",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "ecc440b122e8e1af9463",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.147fc931.chunk.css"
+  },
+  {
+    "revision": "a89d92ee307836b62b1c",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.a2f2a279.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.a2f2a279.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecc440b122e8e1af9463",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.25caebd9.chunk.js"
+  },
+  {
+    "revision": "8b583280b34de9d01efa",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.8935b325.js"
+  }
+]);
